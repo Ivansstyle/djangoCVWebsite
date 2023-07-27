85c366bd2501951ce6992069858ffaf5ed41dc85
IS <ivansstyle@me.com>
2023-07-27 22:10:20 +0300

    Update file storage location in models.py
    The previous location 'mediafiles/cv' has been changed to 'media'. This is following a structure redesign that centralises all stored files under the 'media' directory instead of individual directories. This improves file management and retrieval.

--- main/models.py
+++ main/models.py
@@ -8,7 +8,7 @@ from django.core.files.storage import FileSystemStorage
 import os
 
 # Create your models here.
-file_storage = FileSystemStorage(location='mediafiles/cv')
+file_storage = FileSystemStorage(location='media')
 
 class Skill(models.Model):
     class Meta:
